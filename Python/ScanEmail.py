9919b50f26b09e4dee214c450a2a5813af2c50cd
Fou <therealjo3d3su@gmail.com>
2025-09-26 23:35:29 +0800

    seperated scansubject and scanbody to separate functions, looks cleaner i think

--- Python/ScanEmail.py
+++ Python/ScanEmail.py
@@ -1,5 +1,7 @@
 r'''
-Scanning email module
+Scanning Email module
+will be the place where it scans the email for suspicious words
+and returns a risk score based on the findings
 
 '''
 
@@ -8,9 +10,25 @@ from email.header import decode_header
 import os
 
 
+
+suspiciouswords = [] 
+riskScoreSubject = riskScoreBody = 0
+
 def ScanEmail(email: mailbox.mboxMessage, urls: list=[])->int:
     '''Scan the email for suspicious words if there are suspicious words found it would check if it is the first 100 words of the email body.
     Decode the subject line of the email.
+    '''
+    
+
+    ScanSubject(email)
+    ScanBody(email)
+
+    return riskScoreSubject + riskScoreBody
+    #scanURLs(urls)
+
+def ScanSubject(email: mailbox.mboxMessage):
+    '''Scan the subject of the email for suspicious words, 
+    adding to the risk score when necessary
     '''
     #Decodes the subject line. If it does not need to be decoded it will still pass
     #risk score for subject and position based on the word doc
@@ -18,15 +36,24 @@ def ScanEmail(email: mailbox.mboxMessage, urls: list=[])->int:
     subject = decode_header(subject)
     subject = ''.join(part.decode(charset or 'utf-8') if isinstance(part, bytes) else part for part, charset in subject)
     subject = subject.lower()
-    #print(subject)
-    riskScore = 0
+    print(subject)
+
+    global riskScoreSubject
 
     print(f"\n\nIn subject line:")
     for i in suspiciouswords:
         if i in subject:
             print(f"Found suspicious word '{i}' in subject line.")
-            riskScore += 15
+            riskScoreSubject += 15
+    pass
 
+def ScanBody(email: mailbox.mboxMessage):
+    '''Scan the body of the email for suspicious words, 
+    adding to the risk score when necessary
+    '''
+
+
+    global riskScoreBody
     print(f"\n\nIn email body:")
     
     for word in suspiciouswords:
@@ -36,7 +63,7 @@ def ScanEmail(email: mailbox.mboxMessage, urls: list=[])->int:
             print(f"Found suspicious word '{word}' in email.")
             # say out the word found
             
-            riskScore += 1
+            riskScoreBody += 1
             #place holder until we decide how riskScoring will work
 
             first_100_words = email.get_payload().split() 
@@ -45,10 +72,8 @@ def ScanEmail(email: mailbox.mboxMessage, urls: list=[])->int:
             for position in first_100_words:
                 if word == position:
                     print(f"'{word}' is in the first 100 words of the email.\n")
-                    riskScore += 10
-    return riskScore
-    #scanURLs(urls)
-
+                    riskScoreBody += 10
+    pass
 
 def SetSuspiciousWords(path: str):
     '''Set the list of suspicious words
@@ -59,11 +84,3 @@ def SetSuspiciousWords(path: str):
     file.close()
     pass
 
-suspiciouswords = [] 
-
-if __name__=='__main__':
-    riskScore = 0
-    #print(os.getcwd())
-                                   #initialize empty list
-    SetSuspiciousWords("sampleWordList.txt")            #set the suspicious words from the file
-    
